2a646ddba71c3b7c817577d515e5535340075191
Timo <timothun@web.de>
2022-04-22 17:58:03 +0200

    psi=0 input estimation in GS NN

--- models.py
+++ models.py
@@ -34,7 +34,7 @@ class HighBetaMLP(torch.nn.Module):
 class GradShafranovMLP(torch.nn.Module):
     def __init__(
         self,
-        width: int = 32,
+        width: int = 16,
         R0: float = 0.0,
         a: float = 1.0,
         b: float = 1.0,
@@ -43,7 +43,9 @@ class GradShafranovMLP(torch.nn.Module):
         super().__init__()
 
         self.fc1 = torch.nn.Linear(2, width)
+        # self.fc3 = torch.nn.Linear(width, width)
         self.tanh = torch.nn.Tanh()
+        # self.tanh1 = torch.nn.Tanh()
         self.fc2 = torch.nn.Linear(width, 1)
 
         #  Scaling parameters
@@ -64,5 +66,33 @@ class GradShafranovMLP(torch.nn.Module):
         Z = Z / self.b
         #  Compute psi
         psi_hat = self.fc1(torch.stack([R, Z], dim=-1))
+        # psi_hat = self.fc3(self.tanh1(psi_hat))
         psi_hat = self.tanh(psi_hat / 2)
         return self.psi_0 * self.fc2(psi_hat).view(-1)
+
+    def get_zero_psi_input(self, axis_guess):
+        axis_guess.requires_grad_(True)
+        self.requires_grad_(False)
+        optim = torch.optim.LBFGS([axis_guess], lr=0.1)
+        criterion = torch.nn.L1Loss()
+        psi = self.forward(axis_guess)
+        goal = torch.zeros_like(psi)
+
+        def closure():
+            optim.zero_grad()
+            x = self.forward(axis_guess)
+            loss = criterion(x, goal)
+            loss.backward()
+            return loss
+
+        while True:
+            psi_old = psi
+            optim.step(closure)
+            psi = self.forward(axis_guess)
+            if torch.abs(psi) >= 1e-8:
+                break
+            if (psi - psi_old) <= 1e-8:
+                break
+
+        self.requires_grad_(True)
+        return axis_guess.detach()
