added axis loss to inverse GS, model is currently working but normalization using a & b is not finished. The issue of low pde error while wrong axis location persists

models.py
@@ -140,12 +140,16 @@ class InverseGradShafranovMLP(torch.nn.Module):
     def __init__(self,
                  width: int = 16,
                  R0: float = 0.0,
+                 a = 0., b = 0.
                  ) -> None:
         super().__init__()
 
         self.R0 = R0
+        self.a, self.b = a, b
 
-        self.fc1 = torch.nn.Linear(2, width)
+        self.fc0 = torch.nn.Linear(2, width)
+        self.relu = torch.nn.Tanh()
+        self.fc1 = torch.nn.Linear(width, width)
         self.tanh = torch.nn.Tanh()
         self.fc2 = torch.nn.Linear(width, 3)
 
@@ -153,9 +157,29 @@ class InverseGradShafranovMLP(torch.nn.Module):
         torch.nn.init.zeros_(self.fc2.bias)
         # with torch.no_grad():
         #     self.fc2.bias[0].data = self.R0
-        torch.nn.init.normal_(self.fc2.weight, std=6e-1)
+        torch.nn.init.normal_(self.fc2.weight, std=2e-3)
 
-    def forward(self, x: Tensor) -> Tensor:
+        self.forward = self.forward_
+
+    def forward_(self, x: Tensor) -> Tensor:
+        # x = torch.cat((
+        #     x[:, 0].unsqueeze(1),
+        #     torch.sin(x[:, 1]).unsqueeze(1),
+        #     torch.cos(x[:, 1]).unsqueeze(1)
+        # ), dim=1)
+
+        # x = torch.cat((x[:,0].unsqueeze(1),
+        #    (x[:,1]/(torch.pi)).unsqueeze(1)), dim=1)
+
+        out = self.fc0(x)
+        out = self.relu(out / 2)
+        out = self.fc1(out)
+        out = self.tanh(out / 2)
+        out = self.fc2(out)  # * x[:, :1]
+        out[:, 0] = out[:, 0] + self.R0
+        return out
+
+    def forward_sym(self, x: Tensor) -> Tensor:
         # s = x[:, 0]
         # theta = x[:, 1]
 
@@ -165,13 +189,36 @@ class InverseGradShafranovMLP(torch.nn.Module):
         #     torch.cos(x[:, 1]).unsqueeze(1)
         # ), dim=1)
 
-        RZ = self.fc1(x)
-        RZ = self.tanh(RZ / 2)
-        RZ = self.fc2(RZ)  # * x[:, :1]
+        # x = torch.cat((x[:,0].unsqueeze(1),
+        #    (x[:,1]/(torch.pi)).unsqueeze(1)), dim=1)
 
-        RZ[:, 0] = RZ[:, 0] + self.R0
+        #out = x[x[:, 1] % torch.pi != 0]  # only theta > 0 components, upper half plane (Z>0)
+        out = x[x[:, 1] >= 0].clone()
+        idc_z0 = (out[:, 1] % torch.pi == 0).nonzero(as_tuple=True)[0]
+        z0_mask = torch.ones_like(out[:,0], dtype=torch.bool)
+        z0_mask[idc_z0] = 0
+        #out = torch.vstack((out, x[x[:, 1] % torch.pi == 0]))  # points on R,Z=0 axis
+        out = self.fc0(out)
+        out = self.relu(out/2)
+        out = self.fc1(out)
+        out = self.tanh(out/2)
+        out = self.fc2(out)  # * x[:, :1]
 
-        return RZ
+        out[:, 0] = out[:, 0] + self.R0
+
+        # predict 1/(2*Nfp) R,Z,lambda (up-down symmetry) - then mirror
+        idc_neg = (x[:, 1] < 0).nonzero(as_tuple=True)[0]
+        idc_pos = (x[:, 1] >= 0).nonzero(as_tuple=True)[0]
+        out_mirror = torch.zeros(size=(x.shape[0], 3))
+        out_mirror[idc_pos] = out.clone()
+        out_mirror[idc_neg] = out[z0_mask].clone()
+        out_mirror[idc_neg, 1] *= -1  # Z = -Z in stellarator symmetry when flipping theta
+        # out_mirror[idc_0] = out[-len(idc_0):]
+        # out_mirror[idc_neg] = out[:-len(idc_0)]
+        # out_mirror[idc_neg, 1] *= -1
+        # out_mirror[idc_neg] = out
+
+        return out_mirror
 
 
 

train.py
@@ -12,7 +12,7 @@ from plot_utils import plot_solution, plot_grid
 torch.set_default_tensor_type(torch.DoubleTensor)
 
 
-def train(equilibrium: str, nepochs: int, normalized: bool = False, seed: int = 42):
+def train(equilibrium: str, nepochs: int, normalized: bool = False, seed: int = 420):
 
     assert equilibrium in ("high-beta", "grad-shafranov", "inverse-grad-shafranov")
 
@@ -50,14 +50,15 @@ def train(equilibrium: str, nepochs: int, normalized: bool = False, seed: int = 
         model = GradShafranovMLP(**params)
     elif equilibrium == "inverse-grad-shafranov":
         equi = InverseGSEquilibrium(**params)
-        params = {"R0": equi.Rb[0]}
+        params = {"R0": equi.Ra, "a": equi.a, "b": equi.b}
+        # params = {}
         model = InverseGradShafranovMLP(**params)
     else:
         raise NotImplementedError(f"{equilibrium}")
 
     model.train()
 
-    learning_rate = 0.2
+    learning_rate = 0.05
     optimizer = torch.optim.LBFGS(
         model.parameters(),
         lr=learning_rate,
@@ -85,6 +86,9 @@ def train(equilibrium: str, nepochs: int, normalized: bool = False, seed: int = 
 
             x_domain.requires_grad_()
 
+            if equilibrium == "inverse-grad-shafranov":
+                x_axis.requires_grad_()
+
             def closure():
                 optimizer.zero_grad()
                 if equilibrium == "grad-shafranov" or \
@@ -94,8 +98,8 @@ def train(equilibrium: str, nepochs: int, normalized: bool = False, seed: int = 
                         model(x_domain),
                         x_boundary,
                         model(x_boundary),
-                        # x_axis,
-                        # model(x_axis),
+                        x_axis,
+                        model(x_axis),
                     )
                 elif (equilibrium == "high-beta"):
                     loss = equi.closure(
@@ -120,8 +124,8 @@ def train(equilibrium: str, nepochs: int, normalized: bool = False, seed: int = 
                         model(x_domain),
                         x_boundary,
                         model(x_boundary),
-                        # x_axis,
-                        # model(x_axis),
+                        x_axis,
+                        model(x_axis),
                         return_dict=True
                     )
                 elif equilibrium == "high-beta":
@@ -212,8 +216,16 @@ def train(equilibrium: str, nepochs: int, normalized: bool = False, seed: int = 
         x.requires_grad_()
         preds = model(x)
         equi.fluxsurfacesplot(rz, ax, psi=psi, ns=psi.shape[0])
-        equi.fluxplot(preds.detach().numpy(), ax)
-        plot_solution(x, preds, colorvar=1, ax=ax)
+        #equi.fluxplot(preds.detach().numpy(), ax)
+        plot_solution(x, preds, colorvar=1, ax=ax, title="theta")
+        plt.show()
+        fig, ax = plt.subplots(1, 1, tight_layout=True)
+        x = equi.grid(normalized=normalized)
+        x.requires_grad_()
+        preds = model(x)
+        equi.fluxsurfacesplot(rz, ax, psi=psi, ns=psi.shape[0])
+        # equi.fluxplot(preds.detach().numpy(), ax)
+        plot_solution(x, preds, colorvar=0, ax=ax, title="rho")
         plt.show()
 
 
@@ -235,4 +247,4 @@ if __name__ == "__main__":
     #  TODO: add argparse with default configuration
     # train(equilibrium="high-beta", normalized=True, nepochs=200)
     # train(equilibrium="grad-shafranov", normalized=True, nepochs=100)
-    train(equilibrium="inverse-grad-shafranov", nepochs=50)
+    train(equilibrium="inverse-grad-shafranov", nepochs=100)